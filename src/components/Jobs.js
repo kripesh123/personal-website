5a1371ca48e6925ff1b7cfa7961f38653a5395ef
kripesh <kripeshbista3@gmail.com>
2020-09-06 00:33:10 +0545

    Latest job should come first

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -6,7 +6,7 @@ import { Link } from "gatsby"
 
 const query = graphql`
   {
-    allContentfulJob(sort: { fields: id, order: DESC }) {
+    allContentfulJob(sort: { fields: id, order: ASC }) {
       nodes {
         id
         company
