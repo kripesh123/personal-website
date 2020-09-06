7c756af794361b527d111c53d344a7b915197ee8
kripesh <kripeshbista3@gmail.com>
2020-09-06 12:29:40 +0545

    Improve Blog Template Page

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,19 +1,27 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
-import ReactMarkdown from "react-markdown"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import SEO from "../components/SEO"
+import Image from "gatsby-image"
 
 const ComponentName = ({ data }) => {
-  const { content, title, desc } = data.blog
+  const { content, title, description, image } = data.blog
 
   return (
     <Layout>
-      <SEO title={title} description={desc} />
+      <SEO title={title} description={description} />
       <section className="blog-template">
         <div className="section-center">
           <article className="blog-content">
-            <ReactMarkdown source={content.content} />
+            <h1>{title}</h1>
+            <p>{description}</p>
+            {image && (
+              <Image fluid={image.fluid} className="blog-template-img" />
+            )}
+          </article>
+          <article className="blog-content">
+            { documentToReactComponents(content.json)}
           </article>
           <Link to="/blog" className="btn center-btn">
             blog
@@ -28,10 +36,15 @@ export const query = graphql`
   query GetSingleBlog($slug: String) {
     blog: contentfulBlog(slug: { eq: $slug }) {
       content {
-        content
+        json
       }
       title
       description
+      image {
+        fluid {
+            ...GatsbyContentfulFluid
+        }
+      }
     }
   }
 `
