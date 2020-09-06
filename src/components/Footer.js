14673b7d47dc5821e38ea942f91e23c6020370d1
kripesh <kripeshbista3@gmail.com>
2020-09-06 21:07:13 +0545

    Remove assets image. Modify Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <SocialLinks styleClass="footer-links"></SocialLinks>
         <h4>
           copyright&copy;{new Date().getFullYear()}
-          <span> Full Stack Developer - Kripesh  </span> all rights reserved
+          <span> Full Stack Developer - Kripesh Bista  </span> all rights reserved
         </h4>
       </div>
     </footer>
